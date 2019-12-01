f6ae302a436924b5c3436b71089389c54b327b2a
shawngonsalves <gonsalveshawn@gmail.com>
2019-11-30 21:34:33 -0800

    Sound effects on jump for Scene1,2,3 Background sound for scene 2

---
+++ Assets/jump_sound.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jump_sound : MonoBehaviour
+{
+    public AudioSource jump;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            jump.Play();
+    }
+}
